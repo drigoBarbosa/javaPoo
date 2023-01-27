40c6e9f5bdaf7829cf510881f4d24d9de98960c3
Rodrigo Barbosa <rodrigobs8157@gmail.com>
2023-01-27 20:44:24 -0300

    curso JavaPoo

---
+++ aulaPratica02/Aula02/src/aula02/Caneta.java
@@ -0,0 +1,25 @@
+package aula02;
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    
+    void rabiscar(){
+        
+    }
+    
+    void tampar(){
+        
+    }
+    
+    void destampar(){
+        
+    }
+    
+    void status(){
+        System.out.println("Cor Atual: " + this.cor);
+        
+    }
+}
