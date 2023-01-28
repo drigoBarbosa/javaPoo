ee2880d50b74a6cbd66fdba86bfbd3a57f0724b8
Rodrigo Barbosa <rodrigobs8157@gmail.com>
2023-01-28 11:08:06 -0300

    Finalizando aula 04

--- aulaPratica02/Aula02/src/aula02/Aula02.java
+++ aulaPratica02/Aula02/src/aula02/Aula02.java
@@ -4,11 +4,27 @@ package aula02;
 public class Aula02 {
 
     public static void main(String[] args) {
-       Caneta c1 = new Caneta();
+       System.out.println("");
+       System.out.println("Caneta 1: ");
+       System.out.println("");
+       Caneta c1 = new Caneta(); // Estanciando um objeto c1 da classe Caneta
+       c1.modelo = "Bic"; // Atribuindo valores aos atributos de c1
        c1.cor = "azul";
        c1.ponta = 0.5f;
-       c1.tampada = false;
+       c1.tampar(); // Chamando Metódos de c1
        c1.status();
+       
+       // c1.destampar();
+       c1.rabiscar();
+       System.out.println("");
+       System.out.println("Caneta 2: ");
+       System.out.println("");
+       Caneta c2 = new Caneta(); // Estanciando um objeto c2 da mesma classe Caneta
+       c2.modelo = "Compactor"; // Atribuindo valores aos atributos de c2
+       c2.cor = "Vermelho";
+       c2.ponta = 0.5f;
+       c2.status();  // Chamando Metódos de c2
+       c2.rabiscar();
     }
     
 }
--- aulaPratica02/Aula02/src/aula02/Caneta.java
+++ aulaPratica02/Aula02/src/aula02/Caneta.java
@@ -6,20 +6,28 @@ public class Caneta {
     int carga;
     boolean tampada;
     
+    void status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+    }
     void rabiscar(){
-        
+        if (tampada == true) {
+            System.out.println("[ERRO] Não posso rabiscar! ");
+        } else {
+            System.out.println("Estou rabiscando... ");
+        }
     }
     
     void tampar(){
-        
+        this.tampada = true;
     }
     
     void destampar(){
-        
+        this.tampada = false;
     }
     
-    void status(){
-        System.out.println("Cor Atual: " + this.cor);
-        
-    }
+    
 }
